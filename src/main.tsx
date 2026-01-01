d9f0bcf45b6bb9de964591526d22976e41d52fac
Your Name <you@example.com>
2026-01-01 13:10:31 +0800

    HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { HashRouter } from "react-router-dom";
+
 import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>,
 );
