231f95594da64e3bee3f4ecd9bb0a1bd8ddffb13
Your Name <you@example.com>
2026-01-01 13:18:44 +0800

    回復沒有hashrouter的樣子

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 // import "bootstrap/dist/css/bootstrap.css";
 import { useState } from "react";
 import React from "react";
-import { HashRouter } from "react-router-dom";
 
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./App.css";
@@ -16,26 +15,24 @@ function App() {
   const [selectProduct, setSelectProduct] = useState<productType | null>(null);
 
   return (
-    <HashRouter>
-      <div className="container my-5 mx-auto">
-        <div className="row">
-          {/* 左側列表 */}
-          <div className="col-6">
-            <div className="fs-2">產品列表</div>
-            <Table products={products} setSelectProduct={setSelectProduct} />
-          </div>
-          {/* 右側列表 */}
-          <div className="col-6">
-            {selectProduct && (
-              <React.Fragment>
-                <div className="fs-2">商品明細</div>
-                <Card selectProduct={selectProduct} />
-              </React.Fragment>
-            )}
-          </div>
+    <div className="container my-5 mx-auto">
+      <div className="row">
+        {/* 左側列表 */}
+        <div className="col-6">
+          <div className="fs-2">產品列表</div>
+          <Table products={products} setSelectProduct={setSelectProduct} />
+        </div>
+        {/* 右側列表 */}
+        <div className="col-6">
+          {selectProduct && (
+            <React.Fragment>
+              <div className="fs-2">商品明細</div>
+              <Card selectProduct={selectProduct} />
+            </React.Fragment>
+          )}
         </div>
       </div>
-    </HashRouter>
+    </div>
   );
 }
 
